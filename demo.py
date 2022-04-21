180e52d4bc8cc3a9a7d0a97f2db265eb1f408b21
CrazyOp145 <76449388+CrazyOp145@users.noreply.github.com>
2022-04-21 15:31:35 -0400

    From pyaudio to pyttsx3

--- demo.py
+++ demo.py
@@ -1,28 +1,21 @@
 import speech_recognition as sr
 import webbrowser as wb
+import pyttsx3 as pyt
 
 r1 = sr.Recognizer()
-r2 = sr.Recognizer()
-r3 = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print('Say: "Hello"')
-    audio = r3.listen(source)
+def SpeakText(command):
+    engine = pyt.init()
+    engine.say(command)
+    engine.runAndWait()
 
-if 'Hello' in r2.recognize_google(audio):
-    r2 = sr.Recognizer()
-    url = 'https://www.youtube.com/watch?v=sHeJgKBaiAI'
-    with sr.Microphone() as source:
-        print("What do you want fag?")
-        audio = r2.listen(source)
+with sr.Microphone() as source2:
+    r1.adjust_for_ambient_noise(source2, duration=2)
 
-        # Exceptiion
-        try:
-            get = r2.recognize_google(audio)
-            print(get)
-            wb.get().open_new(url+get)
-        except sr.UnknownValueError:
-            print("ERROR, tf you sayyy?")
-        except sr.RequestError as er:
-            print("ERROR".format(er))
+    print("Listening...")
+    audioInput = r1.listen(source2)
 
+    speechText = r1.recognize_google(audioInput)
+    speechText = speechText.lower()
+
+    print("You said " + speechText)
