ba875a2e2ebcb5a83ba6fd3ceb4e39fc6cfa4e24
Oke Amuwha <amuwhao46@gmail.com>
2022-04-22 15:47:47 -0400

    Added calibration message and comments

--- demo.py
+++ demo.py
@@ -2,20 +2,28 @@ import speech_recognition as sr
 import webbrowser as wb
 import pyttsx3 as pyt
 
+# Recognizer
 r1 = sr.Recognizer()
 
+# Text to speech
 def SpeakText(command):
+
+    # Initialize engine
     engine = pyt.init()
     engine.say(command)
     engine.runAndWait()
 
+
 with sr.Microphone() as source2:
+
+    # Calibrating ambient noise
+    print("Calibrating npise threshold")
     r1.adjust_for_ambient_noise(source2, duration=2)
 
     print("Listening...")
     audioInput = r1.listen(source2)
 
     speechText = r1.recognize_google(audioInput)
-    speechText = speechText.lower()
+    speechText = speechText.lower() # Text to speech converter 
 
-    print("You said " + speechText)
+    print("You said: " + speechText)
